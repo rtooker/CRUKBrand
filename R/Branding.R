d17fb7dd621bb3e6aca790a744fa5290eb023096
Robert Tooker <robert.tooker@cancer.org.uk>
2016-11-28 17:24:21 +0000

    added brand colours to line chart

--- R/Branding.R
+++ R/Branding.R
@@ -1,6 +1,7 @@
 #' Get Footnotes Text
 #'
 #' This function returns the footnotes text
+#' @return The footnotes text
 #' @keywords CRUK
 #' @export
 #' @examples
@@ -21,6 +22,7 @@ getFootnotesText <- function() {
 #'
 #' This function returns the footnotes graphical object
 #' @param footNoteText If NA then will use default text
+#' @return The footnotes graphical object
 #' @keywords CRUK
 #' @export
 #' @examples
@@ -39,10 +41,10 @@ getFootnotesGrob <- function(footNoteText=NULL) {
 }
 
 
-
 #' Get CRUK Logo Graphical Objects
 #'
 #' This function returns the CRUK Logo at 25% of the png (bottom right quarter)
+#' @return The CRUK logo graphical object
 #' @keywords CRUK
 #' @export
 #' @examples
@@ -61,6 +63,7 @@ getCRUKLogo <- function() {
 #'
 #' This function takes a plot and applies the CRUK brand and standard footnote text
 #' @param plot the plot
+#' @return A branded plot including footnotes and logo if required
 #' @keywords CRUK
 #' @export
 #' @examples
@@ -69,7 +72,14 @@ getCRUKLogo <- function() {
 applyCRUKBrand <- function (plot) {
   # basic stylings
   theme_set(theme_bw(12))
-  plot <- plot + theme(plot.title = element_text(lineheight=10, face="italic"))
+
+  # palette
+  cbPalette <- c("#2E008B", "#EC008C", "#00B6ED", "A7A8AA", "AB99D1", "F799D1", "99E2F8")
+
+  # plot formatting
+  plot <- plot +
+    theme(plot.title = element_text(lineheight=10, face="italic")) +
+    scale_colour_manual(values=cbPalette)
 
 
   # https://cran.r-project.org/web/packages/gridExtra/vignettes/arrangeGrob.html
